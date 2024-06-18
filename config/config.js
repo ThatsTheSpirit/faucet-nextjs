8770835c018b6f4e8473b86fd5df21ce16ca3cb8
Atmos <1999bag@gmail.com>
2024-06-19 00:06:01 +0300

    feat: added config for .env

---
+++ config/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+    NETWORK: process.env.NEXT_PUBLIC_NETWORK,
+    RPC_URL: process.env.NEXT_PUBLIC_RPC_URL,
+    CONTRACT: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    ALCHEMY_ID: process.env.NEXT_PUBLIC_ALCHEMY_ID,
+};
