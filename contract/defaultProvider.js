a60543567e63576e79c7c9764550b64723e5b706
Atmos <1999bag@gmail.com>
2024-06-24 02:21:09 +0300

    feat: added defaultProvider

---
+++ contract/defaultProvider.js
@@ -0,0 +1,6 @@
+import { AlchemyProvider } from "ethers";
+import { CONFIG } from "@/config/config";
+
+const defaultProvider = new AlchemyProvider(CONFIG.NETWORK, CONFIG.ALCHEMY_ID);
+
+export default defaultProvider;
