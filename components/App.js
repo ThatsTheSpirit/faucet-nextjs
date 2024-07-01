a776980b3b150e12c4db5e5d014399aa5d914d26
Atmos <1999bag@gmail.com>
2024-07-01 14:38:25 +0300

    feat: added Context Provider in App

--- components/App.js
+++ components/App.js
@@ -1,14 +1,17 @@
 "use client";
 import Header from "@/components/Header";
 import Main from "@/components/Main";
+import { Provider } from "./Provider";
 
 const App = () => {
     return (
         <>
-            <div className="page flex flex-col bg-[#1E1E1E] min-h-[100dvh] pt-[20px]">
-                <Header />
-                <Main />
-            </div>
+            <Provider>
+                <div className="page flex flex-col bg-[#1E1E1E] min-h-[100dvh] pt-[20px]">
+                    <Header />
+                    <Main />
+                </div>
+            </Provider>
         </>
     );
 };
