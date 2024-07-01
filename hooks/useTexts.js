e17d098746e1f597e2d8733ab4861b674d633e84
Atmos <1999bag@gmail.com>
2024-07-01 14:38:01 +0300

    feat: added useTexts hook

---
+++ hooks/useTexts.js
@@ -0,0 +1,10 @@
+import { Context } from "@/components/Provider";
+import { useContext } from "react";
+import texts from "@/texts/texts.json";
+
+function useTexts() {
+    const lang = useContext(Context);
+    return texts[lang];
+}
+
+export default useTexts;
