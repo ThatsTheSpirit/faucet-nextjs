1782b2ddd231b3a40a9c6d4b8c0437a3d34e5667
Atmos <1999bag@gmail.com>
2024-07-09 01:08:46 +0300

    chore: moved to folder providers

--- contract/defaultProvider.js
+++
@@ -1,6 +0,0 @@
-import { AlchemyProvider } from "ethers";
-import CONFIG from "@/config/config";
-
-const defaultProvider = new AlchemyProvider(CONFIG.NETWORK, CONFIG.ALCHEMY_ID);
-
-export default defaultProvider;
