fb656d591253c2fbe2f2f3e5d75836e83323f17a
Atmos <1999bag@gmail.com>
2024-07-13 00:28:27 +0300

    feat(LangSelector.js): NextUI Selector added

--- components/LangSelector.js
+++ components/LangSelector.js
@@ -1,26 +1,44 @@
 import { useContext, useMemo } from "react";
 import { Context } from "./Provider";
 import texts from "@/texts/texts.json";
+import { Select, Avatar, SelectItem } from "@nextui-org/react";
 
 function LangSelector() {
     const { lang, setLang } = useContext(Context);
 
     function selectHandler(e) {
-        setLang(e.target.value);
+        const selectedLang = e.target.value;
+        console.log(`Selected language: ${selectedLang}`);
+        setLang(selectedLang);
         localStorage.setItem("lang", e.target.value);
     }
 
     return (
-        <select
-            className="ml-[15px] p-1 rounded-md bg-[#313235] hover:bg-[#414144] shadow-lang-selector active:bg-[#4c4d4f] text-white text-[17px]"
-            onChange={selectHandler}
-            defaultValue="Language"
-        >
-            <option disabled>Language</option>
-            {Object.entries(texts).map((entry) => (
-                <option value={entry[0]}>{entry[1].selector}</option>
-            ))}
-        </select>
+        <div className="h-[10px]">
+            <Select
+                className="w-[160px] ml-[15px] text-[17px] dark"
+                onChange={selectHandler}
+                label="Select lang"
+                size="sm"
+                color="default"
+            >
+                {Object.entries(texts).map((entry) => (
+                    <SelectItem
+                        key={entry[0]}
+                        value={entry[0]}
+                        startContent={
+                            <Avatar
+                                alt={entry[1].selector}
+                                className="w-7 h-6"
+                                src={`https://flagcdn.com/${entry[0]}.svg`}
+                            />
+                        }
+                    >
+                        {entry[1].selector}
+                    </SelectItem>
+                ))}
+            </Select>
+        </div>
     );
 }
 
